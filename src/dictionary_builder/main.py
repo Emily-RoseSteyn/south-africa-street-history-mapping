955db1ad9759199a8968f0565824602f22fca5cc
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2024-02-19 22:11:47 +0200

    Dictionary with exact matching

--- src/dictionary_builder/main.py
+++ src/dictionary_builder/main.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 
 import pandas as pd
-from thefuzz import process
+from tqdm import tqdm
 
 from utils.country_iso_map import COUNTRY_ISO_CODE_NAME_MAP
 from utils.env_variables import MERGED_STREET_DATA_FILE, FUZZY_MATCH_THRESHOLD, TERMS_DICTIONARY
@@ -25,33 +25,46 @@ def build_dictionary(country: str) -> None:
 
     threshold = FUZZY_MATCH_THRESHOLD
 
-    output = []
-    for term in country_terms["name"]:
-
+    output_df = pd.DataFrame(columns=["name", "country", "iso_code", "likelihood"])
+    # Favour exact match over fuzzy
+    for term in tqdm(country_terms["name"]):
+        # TODO: If term not found
+        # Any ways to speed up processing?
         exact_match = terms_df[terms_df["name"] == term]
-        # TODO: Favour exact match over fuzzy
-        print(exact_match)
 
-        matches = process.extractBests(term, terms_df["name"], limit=number_unique_countries, score_cutoff=threshold)
-        print(matches)
-        highest_probability = matches[0][1]
-        for match in matches:
-            probability = match[1]
+        if len(exact_match) == 1:
+            exact_match.assign(likelihood=[100])
 
-            if probability < highest_probability:
-                break
+        # If more than one exact match, work out countries to retain
+        if len(exact_match) > 1:
+            likelihoods = exact_match["frequency"] * 100 / exact_match["frequency"].sum()
+            exact_match.insert(column="likelihood", value=likelihoods, loc=len(exact_match.columns))
+            exact_match = exact_match.sort_values(by=["likelihood"], ascending=False).reset_index(drop=True)
+            threshold_index = exact_match['likelihood'].cumsum().gt(threshold).idxmin() + 1
+            exact_match = exact_match.loc[:threshold_index]
 
-            index = matches[0][2]
-            matching_country = countries_df[index]
-            result = {
-                "term": term,
-                "origin": matching_country,
-                "probability": probability
-            }
-            output.append(result)
-        break
+        output_df = pd.concat([output_df, exact_match], ignore_index=True)
+        #
+        # matches = process.extractBests(term, terms_df["name"], limit=number_unique_countries, score_cutoff=threshold)
+        # print(matches)
+        # highest_probability = matches[0][1]
+        # for match in matches:
+        #     probability = match[1]
+        #
+        #     if probability < highest_probability:
+        #         break
+        #
+        #     index = matches[0][2]
+        #     matching_country = countries_df[index]
+        #     result = {
+        #         "term": term,
+        #         "origin": matching_country,
+        #         "probability": probability
+        #     }
+        #     output.append(result)
 
-    output_df = pd.DataFrame(output)
+    output_df = output_df.rename({"name": "term"}).drop(['frequency'], axis=1)
+
     # Write result to csv
     output_file = Path(os.path.join("output", f"{country}_{TERMS_DICTIONARY}"))
     output_df.to_csv(output_file, index=False)
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,6 +1,10 @@
 import logging
+import warnings
 
 from utils.env_variables import LOGGING_LEVEL
+
+# Suppress FutureWarning messages
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 def get_logger() -> logging.Logger:
