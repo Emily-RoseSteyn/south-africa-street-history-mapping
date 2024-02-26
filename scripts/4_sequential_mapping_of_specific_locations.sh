aeccd133a0d64bb6874e765dd8917abaf7df8788
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2024-02-26 10:09:14 +0200

    Update script

--- scripts/4_sequential_mapping_of_specific_locations.sh
+++ scripts/4_sequential_mapping_of_specific_locations.sh
@@ -1,6 +1,12 @@
-python ./src/mapping/main.py "Johannesburg, South Africa" 30000 1
-python ./src/mapping/main.py "Cape Town, South Africa" 30000 1
-python ./src/mapping/main.py "Durban, South Africa" 30000 1
-python ./src/mapping/main.py "Bloemfontein, South Africa" 30000 1
-python ./src/mapping/main.py "Gqeberha, South Africa" 30000 1
-python ./src/mapping/main.py "Polokwane, South Africa" 30000 1
+python ./src/mapping/main.py "Johannesburg, South Africa" --distance 30000
+python ./src/mapping/main.py "Johannesburg, South Africa" --distance 30000 --language True
+python ./src/mapping/main.py "Cape Town, South Africa" --distance 30000
+python ./src/mapping/main.py "Cape Town, South Africa" --distance 30000 --language True
+python ./src/mapping/main.py "Durban, South Africa" --distance 30000
+python ./src/mapping/main.py "Durban, South Africa" --distance 30000 --language True
+python ./src/mapping/main.py "Bloemfontein, South Africa" --distance 30000
+python ./src/mapping/main.py "Bloemfontein, South Africa" --distance 30000 --language True
+python ./src/mapping/main.py "Gqeberha, South Africa" --distance 30000
+python ./src/mapping/main.py "Gqeberha, South Africa" --distance 30000 --language True
+python ./src/mapping/main.py "Polokwane, South Africa" --distance 30000
+python ./src/mapping/main.py "Polokwane, South Africa" --distance 30000 --language True
