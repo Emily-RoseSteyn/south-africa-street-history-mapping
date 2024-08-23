b09d50c065b596c47cc1ffb61cc3acad13289aaf
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2024-08-23 09:39:15 +0200

    Plotting gauteng

--- scripts/4_sequential_mapping_of_specific_locations.sh
+++ scripts/4_sequential_mapping_of_specific_locations.sh
@@ -1,5 +1,1 @@
-python ./src/mapping/map-e2e.py "Johannesburg, South Africa" --distance 30000 --fig_size 64
-python ./src/mapping/map-e2e.py "Soweto, South Africa" --distance 5000
-python ./src/mapping/map-e2e.py "Sandhurst, Johannesburg, South Africa" --distance 5000
-python ./src/mapping/map-e2e.py "Sandton, South Africa" --distance 5000
-python ./src/mapping/map-e2e.py "Cape Town, South Africa" --distance 30000 --fig_size 64
+python ./src/mapping/map-e2e.py "Midrand, South Africa" --distance 105000 --fig_size 128
